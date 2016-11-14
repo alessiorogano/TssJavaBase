6f65be15af2d67912704365bcf8e3059a2d6381b
tss <tss@debian-tss24>
2016-11-14 12:10:13 +0100

    Creato nuovo file

---
+++ src/Lezione2/Variabili_e_Commenti.java
@@ -0,0 +1,37 @@
+package Lezione2;
+
+/**
+ *
+ * Esempio di tipo di commento
+ *
+ *
+ */
+public class Variabili_e_Commenti {
+
+    /**
+     * programma amma amma
+     */
+    
+    private static int ale;     //variabili chiamate attributi o proprietà
+                                //possono essere public o private
+                                //si può mettere static, altrimenti si chiama di istanza
+                                //args è una variabile parametri del main
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        
+        
+        boolean pippo=true;    //variabili locali
+        
+        while (pippo=true) {
+            System.out.println("Lezione2.Main2.main()");
+        }
+        
+        
+        
+    }
+
+}
